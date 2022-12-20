e7a9126b7f8ed17c4c2df9e560718f23cc0cdea8
TheMajkel01 <michalnl@onet.pl>
2022-12-20 23:33:01 +0100

    Basic trip date display

--- app/src/main/java/com/example/wycieczki/MainActivity.kt
+++ app/src/main/java/com/example/wycieczki/MainActivity.kt
@@ -2,8 +2,19 @@ package com.example.wycieczki
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.CalendarView
 import android.widget.EditText
+import android.widget.TextView
+import org.w3c.dom.Text
+import java.text.SimpleDateFormat
+import java.util.*
+
+fun parseDate(date: String): Long {
+    val formatter = SimpleDateFormat("yyyy-MM-dd", Locale.getDefault())
+    val parsedDate = formatter.parse(date)
+    return parsedDate.time
+}
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,5 +26,22 @@ class MainActivity : AppCompatActivity() {
         val notesEditText = findViewById<EditText>(R.id.notes)
 
         data class Trip(val id: Long, val startDate: Long, val endDate: Long, val tripTime: Long, val destination: String, val notes: String)
+
+        calendarView.setOnDateChangeListener { view, year, month, dayOfMonth ->
+            val selectedDate = "$year-${month + 1}-$dayOfMonth"
+
+            if (findViewById<TextView>(R.id.startDateTextView).text.isEmpty()) {
+                findViewById<TextView>(R.id.startDateTextView).text = selectedDate
+            } else if (findViewById<TextView>(R.id.endDateTextView).text.isEmpty()) {
+                findViewById<TextView>(R.id.endDateTextView).text = selectedDate
+            }
+        }
+
+        findViewById<Button>(R.id.reset).setOnClickListener {
+            findViewById<TextView>(R.id.startDateTextView).text = ""
+            findViewById<TextView>(R.id.endDateTextView).text = ""
+            findViewById<EditText>(R.id.destination).text.clear()
+            findViewById<EditText>(R.id.notes).text.clear()
+        }
     }
 }
