afd37b89f68ed7e908c64266f2eab2d96c8cff49
TheMajkel01 <michalnl@onet.pl>
2022-12-20 21:59:34 +0100

    Cosmetic changes and input UI

--- app/src/main/java/com/example/wycieczki/MainActivity.kt
+++ app/src/main/java/com/example/wycieczki/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.wycieczki
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.CalendarView
+import android.widget.EditText
+import java.time.Duration
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -10,7 +12,9 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val calendarView = findViewById<CalendarView>(R.id.calendarView)
+        val destinationEditText = findViewById<EditText>(R.id.destination)
+        val notesEditText = findViewById<EditText>(R.id.notes)
 
-
+        data class Trip(val id: Long, val startDate: Long, val endDate: Long, val tripTime: Long, val destination: String, val notes: String)
     }
 }
